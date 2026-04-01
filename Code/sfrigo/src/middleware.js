41872028b8825975960ec4aced89f4c5d40f35d0
zodiacapricorn <a.radu@studenti.unibg.it>
2026-04-01 13:57:15 +0200

    Remove print

--- Code/sfrigo/src/middleware.js
+++ Code/sfrigo/src/middleware.js
@@ -1,10 +1,8 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  console.log("Middleware eseguito su:", request.nextUrl.pathname);
   
   const token = request.cookies.get("__session")?.value;
-  console.log("Token presente:", !!token);
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
